c151cdc330a7a066c46c55a42aa79f9c08fd73de
Chojs <jjcs735@naver.com>
2020-09-11 03:16:18 +0900

    text animation 추가, about me와 career  분리

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
  * Copyright 2013-2020 Start Bootstrap
  * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
  */
+
 (function ($) {
   "use strict"; // Start of use strict
 
@@ -50,6 +51,10 @@
     .on("click", function (e) {
       e.preventDefault();
     });
+})(jQuery); // End of use strict
+
+$(document).ready(function () {
+  wordflick();
 
   // project modal open
   $("#project_1").on("click", function (e) {
@@ -66,7 +71,49 @@
     e.preventDefault();
     $("#project_modal_3").modal("show");
   });
-})(jQuery); // End of use strict
+});
+
+var words = ["개발을 좋아하는", "개발을 하는", "저는 개발을 좋아합니다"],
+  part,
+  i = 0,
+  offset = 0,
+  len = words.length,
+  forwards = true,
+  skip_count = 0,
+  skip_delay = 15,
+  speed = 100;
+
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    } else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      } else {
+        offset--;
+      }
+    }
+    $(".word").text(part);
+  }, speed);
+};
 
 /* typing script */
 var typingBool = false;
